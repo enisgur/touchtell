a34faccdd68b31b0737d3fbf71fb9d61c803c755
enisgur <gurenis@outlook.com>
2019-07-20 01:28:52 -0700

    change axios proxy localhost to heroku back

--- src/actions/types.js
+++ src/actions/types.js
@@ -1,5 +1,5 @@
-// export const axiosURL = 'https://touchtell-server.herokuapp.com/';
-export const axiosURL = 'http://localhost:5000/';
+// export const axiosURL = 'http://localhost:5000/';
+export const axiosURL = 'https://touchtell-server.herokuapp.com/';
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
 export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
